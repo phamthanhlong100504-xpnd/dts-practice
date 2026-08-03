f79ff2dd38ae344e935ec2175eed0f41aad64f82
phamthanhlong1005 <166156509+phamthanhlong100504-xpnd@users.noreply.github.com>
2026-08-03 08:49:43 +0700

    update

--- src/main/java/com/dts/practice/DtsPracticeApplication.java
+++ src/main/java/com/dts/practice/DtsPracticeApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 @ConfigurationPropertiesScan
 @EnableScheduling
 public class DtsPracticeApplication {
